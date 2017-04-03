085859cb10c1048d62e3597770d0875ccc8b196b
yunyuzhu <123409427@qq.com>
2017-04-03 22:19:11 +0800

    reason:增加规则应用页面及功能 author:lidechun

---
+++ src/main/webapp/WEB-INF/pages/back/bkRuleApply.jsp
@@ -0,0 +1,156 @@
+﻿<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<script src="app/js/back/bkruleapply.js" type="text/javascript" charset="UTF-8"></script>
+
+<div class="ct-page">
+    <!--列表-->
+    <div class="box no-border">
+        <div class="box-header with-border">
+            <h3 class="boxhd-title">选择待设置规则目标</h3>
+        </div>
+        <div class="box-body">
+            <div class="bstable-tool" id="toolbar">
+                <div class="menutabs" id="applyType">
+                    <a class="menutab on">行业应用</a>
+                    <a class="menutab">批量应用</a>
+                    <a class="menutab">单独应用</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="navcons" id="applyList">
+                        <!--行业列表-->
+                        <div class="navcon on">
+                            <h5>行业列表</h5>
+                            <div class="tradeList">
+                                <form class="form-horizontal" role="form">
+                                    <div class="row">
+                                        <div class="col-xs-5">
+                                            <div class="form-group">
+                                                <label for="trade" class="col-sm-2 control-label">行业</label>
+                                                <div class="col-sm-7">
+                                                    <div class="ct-input-frame">
+                                                        <select id="tradeTab" class="form-control"></select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!--商户列表-->
+                        <div class="navcon">
+                            <div class="box no-border">
+                                <div class="box-body">
+                                    <form class="form-horizontal" role="form">
+                                        <div class="row">
+                                            <div class="col-xs-4">
+                                                <div class="form-group">
+                                                    <label for="trade" class="col-sm-2 control-label">行业</label>
+                                                    <div class="col-sm-8">
+                                                        <div class="ct-input-frame">
+                                                            <select id="trade" class="form-control"></select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-xs-4">
+                                                <div class="form-group">
+                                                    <label for="name" class="col-sm-3 control-label">商户名称</label>
+                                                    <div class="col-sm-8">
+                                                        <div class="ct-input-frame">
+                                                            <input type="text" class="form-control" id="name" value="" placeholder="输入商户名称">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="box-footer no-border">
+                                    <a class="btn ctbtn" id="shopSubmit">查询商户</a>
+                                </div>
+                            </div>
+                            <h5>商户列表</h5>
+                            <table class="table" id="listTable">
+                                <thead>
+                                <tr>
+                                    <th class="col0"></th>
+                                    <th class="col1">序号</th>
+                                    <th class="col2">商户名称</th>
+                                    <th class="col3">行业</th>
+                                    <th class="col4">联系人</th>
+                                    <th class="col5">联系人电话</th>
+                                </tr>
+                                </thead>
+                                <tbody class="listbody"></tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--规则列表-->
+    <div class="box no-border">
+        <div class="box-header with-border">
+            <h3 class="boxhd-title">选择待应用规则</h3>
+        </div>
+        <div class="box-body">
+            <!--输入部分-->
+            <div class="box no-border">
+                <div class="box-body">
+                    <form class="form-horizontal" role="form">
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <div class="form-group">
+                                    <label for="startTime" class="col-sm-3 control-label">开始时间</label>
+                                    <div class="col-sm-8">
+                                        <div class="ct-input-frame">
+                                            <input type="text" class="form-control" id="startTime" value="" placeholder="输入开始时间">
+                                            <i class="fa fa-calendar"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xs-4">
+                                <div class="form-group">
+                                    <label for="endTime" class="col-sm-3 control-label">结束时间</label>
+                                    <div class="col-sm-8">
+                                        <div class="ct-input-frame">
+                                            <input type="text" class="form-control" id="endTime" value="" placeholder="输入结束时间">
+                                            <i class="fa fa-calendar"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="box-footer no-border">
+                    <a class="btn ctbtn" id="ruleSubmit">查询规则</a>
+                </div>
+            </div>
+            <h5>规则列表</h5>
+            <table class="table" id="ruleTable">
+                <thead>
+                <tr>
+                    <th class="col0"></th>
+                    <th class="col1">序号</th>
+                    <th class="col2">规则名称</th>
+                    <th class="col3">兑换率</th>
+                    <th class="col4">备注</th>
+                    <th class="col5">创建时间</th>
+                </tr>
+                </thead>
+                <tbody class="listbody"></tbody>
+            </table>
+        </div>
+        <!--应用规则-->
+        <div class="box-footer no-border">
+            <a id="ruleApply" class="btn btn-primary">应用规则</a>
+        </div>
+    </div>
+</div>
---
+++ src/main/webapp/app/js/back/bkruleapply.js
@@ -0,0 +1,360 @@
+/**
+ * Created by chun
+ */
+var tmpIdArr;
+var tmpRuleIdArr;
+//选中行业
+var tradeFlag = true;
+var checkboxFlag = true;
+//加载页面
+function loadhtml(){
+    //行业应用列表
+    TradeList({id: "#tradeTab"});
+    //加载行业列表
+    TradeList();
+    //时间输入
+    startEndTimeInit($("#startTime"), $("#endTime"));
+
+    //菜单切换
+    $("#applyType").on('click', 'a', function(){
+        var on = "on";
+        var $tabs = $("#applyType").find('a');
+        var index = $tabs.index(this);
+        applyNavGo(index);
+    });
+    applyNavGo(0);
+
+    //商户筛选
+    $("#shopSubmit").on('click', function(){
+        listTableFun(checkboxFlag);
+    });
+
+    //规则筛选
+    $("#ruleSubmit").on('click', function(){
+        ruleTableFun();
+    });
+    ruleTableFun();
+
+    //规则应用
+    $("#ruleApply").on('click', function(){
+        ruleApply();
+    });
+
+    //窗口大小调整
+    $(window).on('resize', function(){
+        setTimeout(function(){
+            $("#listTable").bootstrapTable('resetView');
+        }, 300);
+    });
+}
+$(document).ready(function(){
+    loadhtml();
+});
+
+//菜单切换
+function applyNavGo(index){
+    var $tableElem = $("#listTable");
+    var on = "on";
+    var $tabs = $("#applyType").find('a');
+    var $cons = $("#applyList").find('.navcon');
+    var indexCon = index;
+    $tabs.removeClass(on).eq(index).addClass(on);
+    if(index != 0){
+        indexCon = 1;
+    }
+    $cons.removeClass(on).eq(indexCon).addClass(on);
+
+    switch(index){
+        case 0:
+            tradeFlag = true;
+            break;
+        case 1:
+            checkboxFlag = true;
+            tradeFlag = false;
+            // $tableElem.bootstrapTable('refreshOptions', listTableColumns(checkboxFlag));
+            listTableFun(checkboxFlag);
+            break;
+        case 2:
+            checkboxFlag = false;
+            tradeFlag = false;
+            listTableFun(checkboxFlag);
+            // $tableElem.bootstrapTable('refreshOptions', listTableColumns(checkboxFlag));
+            break;
+    }
+}
+
+/*************  商户列表  ****************/
+function listTableColumns(flag){
+    var defFlag = true;
+    var columns = [
+        { field: 0, width: "10%", align: 'center', valign: 'middle', halign: 'center', sortable: false,
+            checkbox: true, radio: false },
+        { field: 1, width: "10%", align: 'center', valign: 'middle', halign: 'center', sortable: false },
+        { field: 2, width: "20%", align: 'center', valign: 'middle', halign: 'center', sortable: false },
+        { field: 3, width: "20%", align: 'center', valign: 'middle', halign: 'center', sortable: false },
+        { field: 4, width: "20%", align: 'center', valign: 'middle', halign: 'center', sortable: false },
+        { field: 5, width: "20%", align: 'center', valign: 'middle', halign: 'center', sortable: false }
+    ];
+    if(typeof(flag) != 'undefined'){
+        defFlag = flag;
+    }
+    if(!defFlag){
+        columns[0].checkbox = false;
+        columns[0].radio = true;
+    }
+    return columns;
+}
+//列表
+function listTableFun(flag){
+    //输入参数
+    function getInData(params){
+        var inData = {};
+        inData.trade = $("#trade").val();
+        inData.tenantName = $("#name").val();
+        //表格参数
+        if(typeof(params) != 'undefined'){
+            inData.pageSize  = params.limit;
+            inData.pageStart = params.offset;
+        }
+        return inData;
+    }
+    /************ 表格 **************/
+    // 表格
+    function drawTable(){
+        var $tableElem = $("#listTable");
+        var classes = 'table table-hover tableStyle';
+        //返回数据
+        function resHandler(res) {
+            var dataObj = {"rows": [], "total": 0};
+            var jsonData = eval(res);
+            var dataRows  = jsonData['rows'];
+            var dataTotal = jsonData['total'];
+            if(dataTotal > 0) {
+                var idArr = [];
+                var pageSize = dataRows.length;
+                for (var i = 0; i < pageSize; i++) {
+                    var curObj = dataRows[i];
+                    var curArr = [];
+                    curArr[1] = curObj['orderId'];
+                    curArr[2] = curObj['tenantName'];
+                    curArr[3] = curObj['trade'];
+                    curArr[4] = curObj['linkName'];
+                    curArr[5] = curObj['linkPhone'];
+                    dataRows[i] = curArr;
+                    //记录id
+                    idArr[i] = curObj['id'];
+                }
+                tmpIdArr = idArr;
+                dataObj = {"rows": dataRows, "total": dataTotal};
+            }
+            return dataObj;
+        }
+        //输入参数
+        function inParams(params) {
+            var inData = new getInData(params);
+            return inData;
+        }
+        //行属性
+        function rowAttr(row, index){
+            return {
+                'data-id': tmpIdArr[index]
+            };
+        }
+        $tableElem.bootstrapTable('destroy');
+        $tableElem.bootstrapTable({
+            method: 'get',
+            contentType: "application/x-www-form-urlencoded",
+            url: "rule/shopList",
+            cache: false,
+            dataType : 'json',
+            queryParams: inParams,
+            classes: classes,
+            sortOrder: 'asc',
+            striped: true,
+            pagination: true,
+            pageSize: 200,
+            pageList: [10, 25, 50, 100, 200],
+            search: false,
+            showToggle : false,
+            singleSelect : false,
+            sidePagination:'server',    //设置为服务器端分页
+            showColumns: false, //设置为True可显示表格显示/隐藏列表
+            showRefresh: false, //设置为True可显示刷新按钮
+            minimumCountColumns: 2, //表格显示/隐藏列表时可设置最小隐藏的列数
+            clickToSelect: true,    //设置为True时点击行即可选中单选/复选框
+            smartDisplay: true,  //设置为True智能显示分页或者Card View
+            responseHandler: resHandler,    //在加载数据前，可以对返回的数据进行处理，参数包含：res: 返回的数据。
+            rowAttributes: rowAttr,
+            columns: listTableColumns(flag)
+        });
+    }
+    //触发
+    drawTable();
+}
+
+/*************  规则列表  ****************/
+//规则列表
+function ruleTableFun(){
+    //输入参数
+    function getInData(params){
+        var inData = {};
+        var startDateObj = getDate($("#startTime").datepicker('getUTCDate'));
+        inData.startTime = startDateObj.year+'-'+startDateObj.month+'-'+startDateObj.day;
+        var endDateObj = getDate($("#endTime").datepicker('getUTCDate'));
+        inData.endTime = endDateObj.year+'-'+endDateObj.month+'-'+endDateObj.day;
+        //表格参数
+        if(typeof(params) != 'undefined'){
+            inData.pageSize  = params.limit;
+            inData.pageStart = params.offset;
+        }
+        return inData;
+    }
+    /************ 表格 **************/
+    // 表格
+    function drawTable(){
+        var $tableElem = $("#ruleTable");
+        var classes = 'table table-hover tableStyle';
+        //返回数据
+        function resHandler(res) {
+            var dataObj = {"rows": [], "total": 0};
+            var jsonData = eval(res);
+            var dataRows  = jsonData['rows'];
+            var dataTotal = jsonData['total'];
+            if(dataTotal > 0) {
+                var idArr = [];
+                var pageSize = dataRows.length;
+                for (var i = 0; i < pageSize; i++) {
+                    var curObj = dataRows[i];
+                    var curArr = [];
+                    curArr[0] = curObj['flag'];
+                    curArr[1] = curObj['orderId'];
+                    curArr[2] = curObj['ruleName'];
+                    curArr[3] = curObj['rate'];
+                    curArr[4] = curObj['remark'];
+                    curArr[5] = curObj['createTime'];
+                    dataRows[i] = curArr;
+                    //记录id
+                    idArr[i] = curObj['id'];
+                }
+                tmpRuleIdArr = idArr;
+                dataObj = {"rows": dataRows, "total": dataTotal};
+            }
+            return dataObj;
+        }
+        //输入参数
+        function inParams(params) {
+            var inData = new getInData(params);
+            return inData;
+        }
+        //行属性
+        function rowAttr(row, index){
+            return {
+                'data-id': tmpRuleIdArr[index]
+            };
+        }
+        $tableElem.bootstrapTable('destroy');
+        $tableElem.bootstrapTable({
+            method: 'get',
+            contentType: "application/x-www-form-urlencoded",
+            url: "rule/ruleList",
+            cache: false,
+            dataType : 'json',
+            queryParams: inParams,
+            classes: classes,
+            sortOrder: 'asc',
+            striped: true,
+            pagination: true,
+            pageSize: 200,
+            pageList: [10, 25, 50, 100, 200],
+            search: false,
+            showToggle : false,
+            singleSelect : false,
+            sidePagination:'server',    //设置为服务器端分页
+            showColumns: false, //设置为True可显示表格显示/隐藏列表
+            showRefresh: false, //设置为True可显示刷新按钮
+            minimumCountColumns: 2, //表格显示/隐藏列表时可设置最小隐藏的列数
+            clickToSelect: true,    //设置为True时点击行即可选中单选/复选框
+            smartDisplay: true,  //设置为True智能显示分页或者Card View
+            responseHandler: resHandler,    //在加载数据前，可以对返回的数据进行处理，参数包含：res: 返回的数据。
+            rowAttributes: rowAttr,
+            columns: [
+                { field: 0, width: "10%", align: 'center', valign: 'middle', halign: 'center', sortable: false,
+                    checkbox: false, radio: true },
+                { field: 1, width: "10%", align: 'center', valign: 'middle', halign: 'center', sortable: false },
+                { field: 2, width: "20%", align: 'center', valign: 'middle', halign: 'center', sortable: false },
+                { field: 3, width: "15%", align: 'center', valign: 'middle', halign: 'center', sortable: false },
+                { field: 4, width: "20%", align: 'center', valign: 'middle', halign: 'center', sortable: false },
+                { field: 5, width: "25%", align: 'center', valign: 'middle', halign: 'center', sortable: false }
+            ]
+        });
+    }
+    //触发
+    drawTable();
+}
+
+/*************  应用规则  ****************/
+//获取选中列表id
+function getSelectId($elem){
+    var $tableElem = $elem;
+    //id
+    var $rowList = $tableElem.find("tbody tr");
+    var idArr = [];
+    var rowListSize = $rowList.size();
+    for(var i=0;i<rowListSize; i++){
+        var $curRow = $rowList.eq(i);
+        var $checkbox = $curRow.find('input[type="checkbox"]');
+        if($checkbox.is(':checked')){
+            var curId = $curRow.attr("data-id");
+            idArr.push(curId);
+        }
+    }
+    return idArr;
+}
+//应用规则
+function ruleApply(){
+    //参数
+    var inData = {
+        tradeFlag: tradeFlag
+    };
+    //参数校验
+    do{
+        //行业id或者商户id
+        var idArr = [];
+        if(inData.tradeFlag){
+            idArr = [$("#tradeTab").val()];
+        }
+        else{
+            idArr = getSelectId($("#listTable"));
+        }
+        if(idArr.length <= 0){
+            layer.msg('请选择待设置规则的商户', {icon: 0,skin:'layui-layer-lan',title:'提示',time: 1000});
+            break;
+        }
+        //规则id
+        var ruleIdArr = getSelectId($("#ruleTable"));
+        if(ruleIdArr.length <= 0){
+            layer.msg('请选择待应用规则', {icon: 0,skin:'layui-layer-lan',title:'提示',time: 1000});
+            break;
+        }
+        inData.ids = idArr.join(',');
+        inData.ruleId = ruleIdArr.join(',');
+        //发送服务器
+        $.ajax({
+            type: "get",
+            url: "rule/apply",
+            dataType:"json",
+            data: inData,
+            async: false,
+            jsonp: "callback",
+            success:function(data){
+                var jsonData = eval(data);
+                layer.closeAll('page');
+                layer.msg('应用规则成功！', {icon: 1, time: 1000});
+            },
+            error:function(error){
+                console.log(error);
+                layer.msg('应用规则失败！', {icon: 2, time: 1000});
+            }
+        });
+    }while(0);
+}
